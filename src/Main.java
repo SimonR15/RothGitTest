b286c97853e0db9e81595a80a213d27b7b77775c
BT_1N3_34 <sample@bths.edu>
2024-10-25 13:13:10 -0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("hey guys");
+        System.out.println("several weeks ago I asked you, what would you do if you umm... uhhh..... wait hold on ");
+        System.out.println("GET OUT OF THE WAY");
+        System.out.println("THERE ARE SEVERAL WASPS HEADED TO YOUR LOCATION");
+        System.out.println("THERE'S NOT MUCH TIME LEFT I DON'T HAVE TIME TO EXPLAIN JUST COME WITH ME AND ");
+        System.out.println("...");
+        System.out.println("he was eaten by the wasps");
+        System.out.println("Better luck next time");
+        System.out.println("By the way i think you mitght be a horse but i'm not sure yet");
     }
 }
